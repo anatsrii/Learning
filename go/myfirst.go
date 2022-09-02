276a0039c6f0efcc76aefc2e23a5df30839a236e
anatsrii <anat.allsolution@gmail.com>
2022-09-02 14:54:33 +0700

    First Func git add .

---
+++ go/myfirst.go
@@ -0,0 +1,8 @@
+package main // go จะให้ความสำคัญกับ package main ก่อน (เอาง่ายๆคือรันก่อน)
+
+import "fmt"
+
+func main() {
+	// Run code in func main first !!
+	fmt.Println("Hello Go !!") //fmt คือ build in func ส่วน Println คือ method
+}
